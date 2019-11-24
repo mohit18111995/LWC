3c4c94566f4b4ebab7071c9a0e961eb8f1463f39
mohit18111995 <mohit18111995@gmail.com>
2019-11-24 19:14:35 +0530

    Delete lWC1.js

--- lWC1.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement,wire } from 'lwc';
-import getContactList from '@salesforce/apex/LWC1Controller.getContactList';
-export default class LWC1 extends LightningElement {
-    @wire(getContactList) getcon;
-}
